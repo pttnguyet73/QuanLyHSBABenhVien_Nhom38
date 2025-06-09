fd60e3c3a06defe2a36afbb16115b2e754df8687
pttnguyet73 <pnguyet1992@gmail.com>
2025-06-09 23:34:55 +0700

    Fix merge

--- src/HOSOBENHAN/API_HOSOBENHAN/obj/Debug/net8.0/API_HOSOBENHAN.AssemblyInfo.cs
+++ src/HOSOBENHAN/API_HOSOBENHAN/obj/Debug/net8.0/API_HOSOBENHAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_HOSOBENHAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523a83b303c5917f94d20d81c299ac977bbd1e4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2735e609b770e21f5adad34b49fb5cc51fe6882")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_HOSOBENHAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_HOSOBENHAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

99b9eb28f656c9800cd65d5c6d581ec5164a88e7
pttnguyet73 <pnguyet1992@gmail.com>
2025-06-09 23:37:26 +0700

    Add file

--- src/HOSOBENHAN/HOSOBENHAN/obj/Debug/net8.0/HOSOBENHAN.AssemblyInfo.cs
+++ src/HOSOBENHAN/HOSOBENHAN/obj/Debug/net8.0/HOSOBENHAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HOSOBENHAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2735e609b770e21f5adad34b49fb5cc51fe6882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd60e3c3a06defe2a36afbb16115b2e754df8687")]
 [assembly: System.Reflection.AssemblyProductAttribute("HOSOBENHAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HOSOBENHAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
